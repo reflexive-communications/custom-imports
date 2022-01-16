b49ebccd1cb020c6a56da13ab5045c7e005b1cf2
akosgarai <szlem01@gmail.com>
2022-01-16 16:32:39 +0100

    Contact map inheritance and template

--- CRM/CustomImports/Import/CustomField/Form/ContactMap.php
+++ CRM/CustomImports/Import/CustomField/Form/ContactMap.php
@@ -1,5 +1,5 @@
 <?php
 
-class CRM_CustomImports_Import_CustomField_Form_ContactMap extends CRM_Import_Form_MapField
+class CRM_CustomImports_Import_CustomField_Form_ContactMap extends CRM_Contribute_Import_Form_MapField
 {
 }
