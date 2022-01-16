031a865efd85c6ed4c50c89b98f5266591fdb1aa
akosgarai <szlem01@gmail.com>
2022-01-16 21:28:13 +0100

    Run php-cs-fixer tool on the codebase

--- CRM/CustomImports/Import/CustomField/Controller.php
+++ CRM/CustomImports/Import/CustomField/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
-class CRM_CustomImports_Import_CustomField_Controller extends CRM_Core_Controller {
+class CRM_CustomImports_Import_CustomField_Controller extends CRM_Core_Controller
+{
 
   /**
    * Class constructor.
@@ -9,19 +10,19 @@ class CRM_CustomImports_Import_CustomField_Controller extends CRM_Core_Controlle
    * @param bool|int $action
    * @param bool $modal
    */
-  public function __construct($title = NULL, $action = CRM_Core_Action::NONE, $modal = TRUE) {
-    parent::__construct($title, $modal);
+    public function __construct($title = null, $action = CRM_Core_Action::NONE, $modal = true)
+    {
+        parent::__construct($title, $modal);
 
-    set_time_limit(0);
+        set_time_limit(0);
 
-    $this->_stateMachine = new CRM_CustomImports_Import_CustomField_StateMachine($this, $action);
+        $this->_stateMachine = new CRM_CustomImports_Import_CustomField_StateMachine($this, $action);
 
-    // create and instantiate the pages
-    $this->addPages($this->_stateMachine, $action);
+        // create and instantiate the pages
+        $this->addPages($this->_stateMachine, $action);
 
-    // add all the actions
-    $config = CRM_Core_Config::singleton();
-    $this->addActions($config->uploadDir, ['uploadFile']);
-  }
-
+        // add all the actions
+        $config = CRM_Core_Config::singleton();
+        $this->addActions($config->uploadDir, ['uploadFile']);
+    }
 }
