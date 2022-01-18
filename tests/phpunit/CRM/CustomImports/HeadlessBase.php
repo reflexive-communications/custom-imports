371a08f80eebbbd3c2e56b51f012c0c957df5ac2
akosgarai <szlem01@gmail.com>
2022-01-18 18:26:49 +0100

    Test helper function for creating custom fields

--- tests/phpunit/CRM/CustomImports/HeadlessBase.php
+++ tests/phpunit/CRM/CustomImports/HeadlessBase.php
@@ -5,6 +5,7 @@ use Civi\Test\HeadlessInterface;
 use Civi\Test\HookInterface;
 use Civi\Test\TransactionalInterface;
 use Civi\Test\CiviEnvBuilder;
+use Civi\Api4\CustomGroup;
 
 /**
  * Base testclass to eliminate the code duplication.
@@ -14,17 +15,18 @@ use Civi\Test\CiviEnvBuilder;
 class CRM_CustomImports_HeadlessBase extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface
 {
 
-  /**
-   * Setup used when HeadlessInterface is implemented.
-   *
-   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-   *
-   * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
-   *
-   * @return \Civi\Test\CiviEnvBuilder
-   *
-   * @throws \CRM_Extension_Exception_ParseException
-   */
+    protected static $index = 1;
+    /**
+     * Setup used when HeadlessInterface is implemented.
+     *
+     * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+     *
+     * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+     *
+     * @return \Civi\Test\CiviEnvBuilder
+     *
+     * @throws \CRM_Extension_Exception_ParseException
+     */
     public function setUpHeadless(): CiviEnvBuilder
     {
         return \Civi\Test::headless()
@@ -40,5 +42,39 @@ class CRM_CustomImports_HeadlessBase extends \PHPUnit\Framework\TestCase impleme
     public function tearDown():void
     {
         parent::tearDown();
+    }
+
+    /**
+     * Helper function for creating a brand new custom group and custom field
+     * that could be used for contact mapping.
+     *
+     * @param string $extends
+     * @param string $dataType
+     * @param string $htmlType
+     *
+     * @return array
+     */
+    protected function createCustomField(string $extends, string $dataType, string $htmlType): array
+    {
+        $customGroup = CustomGroup::create(false)
+            ->addValue('title', 'Test custom group '.self::$index)
+            ->addValue('extends', $extends)
+            ->addValue('is_active', 1)
+            ->addValue('is_public', 1)
+            ->addValue('style', 'Inline')
+            ->execute()
+            ->first();
+        $customField = CustomField::create(false)
+            ->addValue('custom_group_id', $customGroup['id'])
+            ->addValue('label', 'Field label v'.self::$index)
+            ->addValue('data_type', $dataType)
+            ->addValue('html_type', $htmlType)
+            ->execute()
+            ->first();
+        self::$index += 1;
+        return [
+            'field' => $customField,
+            'group' => $customGroup,
+        ];
     }
 }
