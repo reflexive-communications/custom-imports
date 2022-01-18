16768911bac4ea0a6a669899cdbed8f8fdd97b50
akosgarai <szlem01@gmail.com>
2022-01-18 20:39:36 +0100

    Test skeleton for the parser application

--- tests/phpunit/CRM/CustomImports/Contribution/CustomField/ParserTest.php
+++ tests/phpunit/CRM/CustomImports/Contribution/CustomField/ParserTest.php
@@ -1,70 +1,56 @@
 <?php
 
 use CRM_CustomImports_ExtensionUtil as E;
-use Civi\Test\HeadlessInterface;
-use Civi\Test\HookInterface;
-use Civi\Test\TransactionalInterface;
-use Civi\Test\CiviEnvBuilder;
 
 /**
- * FIXME - Add test description.
- *
- * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ * Testcases for the parser application.
  *
  * @group headless
  */
-class CRM_CustomImports_Contribution_CustomField_ParserTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface
+class CRM_CustomImports_Contribution_CustomField_ParserTest extends CRM_CustomImports_HeadlessBase
 {
-
-  /**
-   * Setup used when HeadlessInterface is implemented.
-   *
-   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-   *
-   * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
-   *
-   * @return \Civi\Test\CiviEnvBuilder
-   *
-   * @throws \CRM_Extension_Exception_ParseException
-   */
-    public function setUpHeadless(): CiviEnvBuilder
+    /**
+     * init test case.
+     */
+    public function testInit()
     {
-        return \Civi\Test::headless()
-      ->installMe(__DIR__)
-      ->apply();
-    }
-
-    public function setUp():void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown():void
-    {
-        parent::tearDown();
+        $mapperKeys = [];
+        $mapperSoftCredit = [];
+        $mapperPhoneType = null;
+        $mapperSoftCreditType = [];
+        $parser = new CRM_CustomImports_Contribution_CustomField_Parser($mapperKeys, $mapperSoftCredit, $mapperPhoneType, $mapperSoftCreditType);
+        self::assertEmpty($parser->init(), 'Init supposed to be empty.');
     }
 
     /**
-     * Example: Test that a version is returned.
+     * summary test case.
      */
-    public function testWellFormedVersion():void
+    public function testSummary()
     {
-        $this->assertNotEmpty(E::SHORT_NAME);
-        $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
+        $mapperKeys = [];
+        $mapperSoftCredit = [];
+        $mapperPhoneType = null;
+        $mapperSoftCreditType = [];
+        $values = [];
+        $parser = new CRM_CustomImports_Contribution_CustomField_Parser($mapperKeys, $mapperSoftCredit, $mapperPhoneType, $mapperSoftCreditType);
+        self::assertEmpty($parser->init(), 'Init supposed to be empty.');
+        $summary = $parser->summary($values);
+        self::assertBool($summary, 'summary supposed to be something bool.');
     }
 
     /**
-     * Example: Test that we're using a fake CMS.
+     * import test case.
      */
-    public function testWellFormedUF():void
+    public function testImport()
     {
-        $this->assertEquals('UnitTests', CIVICRM_UF);
+        $mapperKeys = [];
+        $mapperSoftCredit = [];
+        $mapperPhoneType = null;
+        $mapperSoftCreditType = [];
+        $values = [];
+        $parser = new CRM_CustomImports_Contribution_CustomField_Parser($mapperKeys, $mapperSoftCredit, $mapperPhoneType, $mapperSoftCreditType);
+        self::assertEmpty($parser->init(), 'Init supposed to be empty.');
+        $isImported = $parser->import(CRM_Import_Parser::DUPLICATE_SKIP, $values);
+        self::assertBool($isImported, 'import supposed to be something bool.');
     }
 }
