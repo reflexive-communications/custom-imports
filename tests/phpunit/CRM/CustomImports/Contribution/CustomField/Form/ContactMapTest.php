41ccc18c88bb96cac088cbb1452fb880c47bfea4
akosgarai <szlem01@gmail.com>
2022-01-18 20:26:07 +0100

    Test skeleton for the map form

--- tests/phpunit/CRM/CustomImports/Contribution/CustomField/Form/ContactMapTest.php
+++ tests/phpunit/CRM/CustomImports/Contribution/CustomField/Form/ContactMapTest.php
@@ -1,70 +1,49 @@
 <?php
 
 use CRM_CustomImports_ExtensionUtil as E;
-use Civi\Test\HeadlessInterface;
-use Civi\Test\HookInterface;
-use Civi\Test\TransactionalInterface;
-use Civi\Test\CiviEnvBuilder;
 
 /**
- * FIXME - Add test description.
- *
- * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ * Testcases for the mapping form.
  *
  * @group headless
  */
-class CRM_CustomImports_Contribution_CustomField_Form_ContactMapTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface
+class CRM_CustomImports_Contribution_CustomField_Form_ContactMapTest extends CRM_CustomImports_HeadlessBase
 {
-
-  /**
-   * Setup used when HeadlessInterface is implemented.
-   *
-   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-   *
-   * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
-   *
-   * @return \Civi\Test\CiviEnvBuilder
-   *
-   * @throws \CRM_Extension_Exception_ParseException
-   */
-    public function setUpHeadless(): CiviEnvBuilder
+    /**
+     * preProcess test case.
+     */
+    public function testPreProcess()
     {
-        return \Civi\Test::headless()
-      ->installMe(__DIR__)
-      ->apply();
-    }
-
-    public function setUp():void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown():void
-    {
-        parent::tearDown();
+        $form = new CRM_CustomImports_Contribution_CustomField_Form_ContactMap();
+        self::assertEmpty($form->preProcess(), 'PreProcess supposed to be empty.');
     }
 
     /**
-     * Example: Test that a version is returned.
+     * formRule test case.
      */
-    public function testWellFormedVersion():void
+    public function testFormRule()
     {
-        $this->assertNotEmpty(E::SHORT_NAME);
-        $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
+        $form = new CRM_CustomImports_Contribution_CustomField_Form_ContactMap();
+        $fields = [];
+        $files = [];
+        self::assertArray(CRM_CustomImports_Contribution_CustomField_Form_ContactMap::formRule($fields, $files, $form), 'It has to return error array');
     }
 
     /**
-     * Example: Test that we're using a fake CMS.
+     * addRules test case.
      */
-    public function testWellFormedUF():void
+    public function testAddRules()
     {
-        $this->assertEquals('UnitTests', CIVICRM_UF);
+        $form = new CRM_CustomImports_Contribution_CustomField_Form_ContactMap();
+        self::assertEmpty($form->addRules(), 'AddRules supposed to be empty.');
+    }
+
+    /**
+     * buildQuickForm test case.
+     */
+    public function testBuildQuickForm()
+    {
+        $form = new CRM_CustomImports_Contribution_CustomField_Form_ContactMap();
+        self::assertEmpty($form->buildQuickForm(), 'BuildQuickForm supposed to be empty.');
     }
 }
