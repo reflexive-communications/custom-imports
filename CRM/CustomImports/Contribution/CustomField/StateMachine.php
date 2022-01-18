a46f8deb1f9c019cb0140a034f8ecd649c77d890
akosgarai <szlem01@gmail.com>
2022-01-18 19:00:52 +0100

    Refactor StateMachine use pages as constants

--- CRM/CustomImports/Contribution/CustomField/StateMachine.php
+++ CRM/CustomImports/Contribution/CustomField/StateMachine.php
@@ -6,23 +6,24 @@
  */
 class CRM_CustomImports_Contribution_CustomField_StateMachine extends CRM_Core_StateMachine
 {
+    public const PAGES = [
+        'CRM_Contribute_Import_Form_DataSource' => null,
+        'CRM_CustomImports_Contribution_CustomField_Form_ContactMap' => null,
+        'CRM_CustomImports_Contribution_CustomField_Form_Preview' => null,
+        'CRM_Contribute_Import_Form_Summary' => null,
+    ];
 
-  /**
-   * Class constructor.
-   *
-   * @param object $controller
-   * @param \const|int $action
-   */
+    /**
+     * Class constructor.
+     *
+     * @param object $controller
+     * @param \const|int $action
+     */
     public function __construct($controller, $action = CRM_Core_Action::NONE)
     {
         parent::__construct($controller, $action);
 
-        $this->_pages = [
-            'CRM_Contribute_Import_Form_DataSource' => null,
-            'CRM_CustomImports_Contribution_CustomField_Form_ContactMap' => null,
-            'CRM_CustomImports_Contribution_CustomField_Form_Preview' => null,
-            'CRM_Contribute_Import_Form_Summary' => null,
-        ];
+        $this->_pages = self::PAGES;
 
         $this->addSequentialPages($this->_pages, $action);
     }
