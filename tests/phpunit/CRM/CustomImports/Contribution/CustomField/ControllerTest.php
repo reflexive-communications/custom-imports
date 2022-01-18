0397e9f9b25eb37a49d5366fae349a9c74027187
akosgarai <szlem01@gmail.com>
2022-01-18 18:52:14 +0100

    Implement controller test case

--- tests/phpunit/CRM/CustomImports/Contribution/CustomField/ControllerTest.php
+++ tests/phpunit/CRM/CustomImports/Contribution/CustomField/ControllerTest.php
@@ -1,70 +1,22 @@
 <?php
 
 use CRM_CustomImports_ExtensionUtil as E;
-use Civi\Test\HeadlessInterface;
-use Civi\Test\HookInterface;
-use Civi\Test\TransactionalInterface;
-use Civi\Test\CiviEnvBuilder;
 
 /**
- * FIXME - Add test description.
- *
- * Tips:
- *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
- *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
- *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
- *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
- *    If this test needs to manipulate schema or truncate tables, then either:
- *       a. Do all that using setupHeadless() and Civi\Test.
- *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ * Testcases for the controller class.
  *
  * @group headless
  */
-class CRM_CustomImports_Contribution_CustomField_ControllerTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface
+class CRM_CustomImports_Contribution_CustomField_ControllerTest extends CRM_CustomImports_HeadlessBase
 {
-
-  /**
-   * Setup used when HeadlessInterface is implemented.
-   *
-   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-   *
-   * @link https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
-   *
-   * @return \Civi\Test\CiviEnvBuilder
-   *
-   * @throws \CRM_Extension_Exception_ParseException
-   */
-    public function setUpHeadless(): CiviEnvBuilder
-    {
-        return \Civi\Test::headless()
-      ->installMe(__DIR__)
-      ->apply();
-    }
-
-    public function setUp():void
-    {
-        parent::setUp();
-    }
-
-    public function tearDown():void
-    {
-        parent::tearDown();
-    }
-
     /**
-     * Example: Test that a version is returned.
+     * It tests the class constructor.
+     * The state machine has to be our custom one.
      */
-    public function testWellFormedVersion():void
+    public function testConstructor():void
     {
-        $this->assertNotEmpty(E::SHORT_NAME);
-        $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
-    }
-
-    /**
-     * Example: Test that we're using a fake CMS.
-     */
-    public function testWellFormedUF():void
-    {
-        $this->assertEquals('UnitTests', CIVICRM_UF);
+        $controller = new CRM_CustomImports_Contribution_CustomField_Controller();
+        $sm = $controller->getStateMachine();
+        self::assertSame('CRM_CustomImports_Contribution_CustomField_StateMachine', get_class($sm));
     }
 }
